f8da9a1fe012daa567fd024bc2b5ca9c2dfb1b91
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-09-05 11:09:37 -0300

    Importando os objetos de rotas + componentes criados

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Bloco de ROtas
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from '.routesHome.jsx';
+import Produtos from '.routesHome.jsx';
+import EditarProdutos from '.routesHome.jsx';
+import Error404 from '.routesHome.jsx';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
